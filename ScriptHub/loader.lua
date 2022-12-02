fd75fe3511e4526055a7fb7f83d11941b8973cf5
edy131109 <lazareduard702@gmail.com>
2022-12-02 22:14:22 +0200

    Update loader.lua

--- ScriptHub/loader.lua
+++ ScriptHub/loader.lua
@@ -2,6 +2,8 @@ local gameID = game.PlaceId
 
 if gameID == 9742783208 then
     loadstring(game:HttpGet("https://pastebin.com/raw/qXLdtXXW"))()
+elseif gameID == 3101667897 then
+    loadstring(game:HttpGet('https://pastebin.com/raw/vAYgbgDL'))()
 else
     loadstring(game:HttpGet("https://pastebin.com/raw/T921JXSw"))()
 end
