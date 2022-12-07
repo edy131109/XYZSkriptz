a7bc65e9131f134a71d310a4dc4d4c0456bf114b
edy131109 <lazareduard702@gmail.com>
2022-12-07 22:42:21 +0200

    Update loader.lua

--- ScriptHub/loader.lua
+++ ScriptHub/loader.lua
@@ -6,6 +6,8 @@ elseif gameID == 3101667897 then
     loadstring(game:HttpGet("https://pastebin.com/raw/vAYgbgDL"))()
 elseif gameID == 11206345614 then
     loadstring(game:HttpGet("https://pastebin.com/raw/ru36ijUx"))()
+elseif gameID == 9984770240 then
+    loadstring(game:HttpGet('https://pastebin.com/raw/eQvLuZrk'))()
 else
     loadstring(game:HttpGet("https://pastebin.com/raw/T921JXSw"))()
 end
